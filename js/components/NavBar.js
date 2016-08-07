7b52b3ed32dcba612492c26be27d5eabd37fe5ca
Matt Bague <mbague@storygize.com>
2016-08-06 21:38:41 -0700

    swap out profile image

--- js/components/NavBar.js
+++ js/components/NavBar.js
@@ -9,7 +9,7 @@ const NavBar = React.createClass({
                   data-target="#bs-example-navbar-collapse-1">
             <span className="sr-only">Toggle navigation</span> Menu <i className="fa fa-bars"/>
           </button>
-          <a className="navbar-brand" href="#page-top">Start Bootstrap</a>
+          <a className="navbar-brand" href="#page-top">Matt Bague</a>
         </div>
         <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
           <ul className="nav navbar-nav navbar-right">
