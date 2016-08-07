67e0f65a4f9e0ef961b0bd50f46b27ae3a40b51c
Matt Bague <mbague@storygize.com>
2016-08-06 21:42:10 -0700

    add Portfolio component

--- js/App.js
+++ js/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import NavBar from 'components/NavBar';
 import Header from 'components/Header';
+import Portfolio from 'components/Portfolio';
 
 const App = React.createClass({
   render () {
     return <div>
       <NavBar/>
       <Header/>
+      <Portfolio/>
     </div>;
   }
 });
