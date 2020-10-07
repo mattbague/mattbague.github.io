176164d7d3221440ffbf73a050069032cfa1d609
Matt Bague <mattbague@gmail.com>
2020-10-07 14:36:46 -0700

    add resume

--- src/main/js/App.tsx
+++ src/main/js/App.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import 'App.css';
+import Resume from "Resume";
 
 export default class App extends React.Component<any, any> {
 
   render() {
-    return <div className="text-center text-3xl font-medium" style={{marginTop: "25vh"}}>Site is getting a makeover. Check back soon :)</div>
+    return <div>
+      <div className="text-center text-3xl font-medium mt-10">Site is getting a makeover. Check back soon!</div>
+      <div className="text-center text-lg my-8">Take a look at my resume in the meantime</div>
+      <Resume/>
+      <div className="text-center mt-8">--</div>
+    </div>
   }
 
 }
---
+++ src/main/js/Resume.tsx
@@ -0,0 +1,178 @@
+import * as React from "react";
+import {GitHub, Globe, Linkedin, Mail} from "react-feather";
+
+export default class Resume extends React.Component<{}, {}> {
+
+  render() {
+    const style = {
+      width: "8.5in",
+      height: "11in",
+    }
+    return <div>
+      <div style={style} className="mx-auto border-solid border border-gray-600">
+        <div style={{margin: ".5in"}}>
+          <div className="flex items-center">
+            <div className="font-bold" style={{width: "40%", fontSize: "3rem"}}>
+              Matt Bague
+            </div>
+            <div className="text-center" style={{width: "60%"}}>
+              <div className="mb-1">
+                <span><Globe className="inline-block" size="1rem"/> <a href="https://mattbague.com" target="_blank">mattbague.com</a></span>
+                <span className="ml-8"><Linkedin className="inline-block" size="1rem"/> <a href="https://www.linkedin.com/in/matthewbague"
+                                                                                           target="_blank">linkedin.com/in/matthewbague</a></span>
+              </div>
+              <div>
+                <span><Mail className="inline-block" size="1rem"/> <a href="mailto:mattbague@gmail.com" target="_blank">mattbague@gmail.com</a></span>
+                <span className="ml-8"><GitHub className="inline-block" size="1rem"/> <a href="https://github.com/mattbague"
+                                                                                         target="_blank">github.com/mattbague</a></span>
+              </div>
+            </div>
+          </div>
+          <div className="flex">
+            <div style={{width: "30%"}}>
+              <Section title={"Education"}>
+                <SectionEntry
+                  title="California Polytechnic State University: San Luis Obispo"
+                  subtitle="College of Engineering"
+                  description={
+                    <div>
+                      BS in Computer Science
+                      <br/>
+                      Class of 2014
+                    </div>
+                  }
+                />
+              </Section>
+              <Section title={"Skills"}>
+                <SectionEntry
+                  subtitle="Languages:"
+                  description="Scala, JavaScript, TypeScript, SQL"
+                />
+                <SectionEntry
+                  subtitle="Frameworks/Libraries:"
+                  description="Spark, React, Kafka, ReactiveX, Avro, AWS Services (Kinesis, S3, etc.)"
+                />
+                <SectionEntry
+                  subtitle="Databases:"
+                  description="Postgres, MemSQL, Scylla/Cassandra, DynamoDB"
+                />
+                <SectionEntry
+                  subtitle="Dev Tools:"
+                  description="Git, IntelliJ, Vim"
+                />
+                <SectionEntry
+                  subtitle="Build Tools:"
+                  description="Docker, Jenkins, Maven, Webpack"
+                />
+                <SectionEntry
+                  subtitle="Environments"
+                  description="Linux, macOS"
+                />
+              </Section>
+              <Section title={"About Me"}>
+                <p>
+                  I've been a full stack engineer since I graduated college back in 2014. Love being able to design and develop a system from the
+                  ground up. Always up for learning something new, whether it's new tech or design patterns.
+                </p>
+              </Section>
+            </div>
+            <div style={{width: "70%"}} className="pl-6">
+              <Section title={"Experience"}>
+                <SectionEntry
+                  title="Storygize (Native Advertising)"
+                  subtitle="Fullstack Software Engineer"
+                  date={"Apr 2016 \u2013 present"}
+                  location="Newbury Park, CA"
+                  description={
+                    <ul className="list-disc pl-6">
+                      <li>
+                        A native advertising demand-side platform startup. Worked with advertisers to serve ads that blend in with the look and feel
+                        of publisher sites and track performance.
+                      </li>
+                      <li>
+                        Worked in all areas of the codebase: web app UI,realtime-bidding logic and servers, tracking servers,reporting/campaign
+                        management API severs, realtime/historical data processing, and build/deployment tooling.
+                      </li>
+                    </ul>
+                  }
+                />
+                <SectionEntry
+                  title="CJ Affiliate (Affiliate Marketing)"
+                  subtitle="Fullstack Software Engineer"
+                  date={"Jun 2014 \u2013 Apr 2016"}
+                  location="Westlake Village, CA"
+                  description={
+                    <ul className="list-disc pl-6">
+                      <li>
+                        Data warehouse: Migrated existing lambda architecture from Flink and Hadoop to Kafka and Spark respectively. Reduced
+                        historical
+                        job run times by 50% and our realtime data throughput significantly.
+                      </li>
+                      <li>
+                        DevOps: Developed a new automated deployment system which relied on ansible playbooks and ran via a Scala app.Reduced
+                        deployment from 1-2 hours (after-hours) to 15 minutes (during work day) and did not require anyone with special knowledge to
+                        be able to deploy our apps.
+                      </li>
+                    </ul>
+                  }
+                />
+              </Section>
+              <Section title={"Projects"}>
+                <SectionEntry
+                  title="CloudBurst"
+                  subtitle="SoundCloud Downloader"
+                  description={
+                    <p>
+                      A CLI downloader built in Scala. Downloads songs with their ID3 tag populated and album art. Reverse engineered their public API
+                      since they do not give out personal API keys anymore.
+                    </p>
+                  }/>
+                <SectionEntry
+                  title="AssetRemix (Unmaintained)"
+                  subtitle={<a href="https://assetremix.herokuapp.com/" target="_blank">assetremix.herokuapp.com</a>}
+                  description={
+                    <p>
+                      A web app focused on providing financial tools and investing guidance. Tools include calculators for tax-efficient
+                      investing, three-fund portfolio allocations, and more.
+                    </p>
+                  }/>
+              </Section>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  }
+}
+
+const Section = (props: { title: string, children?: any }) => {
+  return <div className="mb-2">
+    <div className="font-thin mb-1" style={{fontSize: "1.5rem"}}>
+      {props.title.toUpperCase()}
+    </div>
+    {props.children}
+  </div>
+}
+
+interface SectionEntryProps {
+  title?: string,
+  subtitle?: string | JSX.Element,
+  description?: string | JSX.Element,
+  date?: string
+  location?: string
+}
+
+
+const SectionEntry = (props: SectionEntryProps) => {
+  return <div className="mb-1">
+    <div className="font-bold">
+      {props.title}<span className="float-right font-medium">{props.date}</span>
+    </div>
+    <div className="font-medium">
+      {props.subtitle}<span className="float-right">{props.location}</span>
+    </div>
+    <div>
+      {props.description}
+    </div>
+  </div>
+}
